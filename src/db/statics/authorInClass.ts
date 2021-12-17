febe00ff25c596ca50da63b0e44b519b4d9760d1
seojin kim <001106ksj@gmail.com>
2021-12-17 10:17:52 +0900

    fix: 명단 모두 작가 필명으로 수정

--- src/db/statics/authorInClass.ts
+++ src/db/statics/authorInClass.ts
@@ -3,726 +3,209 @@ const authors = {
     A: {
       professor: '양세준',
       students: [
-        {
-          authorName: '권정현',
-          authorNumber: '1',
-        },
-        {
-          authorName: '김서현',
-          authorNumber: '2',
-        },
-        {
-          authorName: '김양지',
-          authorNumber: '3',
-        },
-        {
-          authorName: '김연수',
-          authorNumber: '4',
-        },
-        {
-          authorName: '김예린',
-          authorNumber: '5',
-        },
-        {
-          authorName: '김혜수',
-          authorNumber: '7',
-        },
-        {
-          authorName: '김평화',
-          authorNumber: '6',
-        },
-        {
-          authorName: '류강희',
-          authorNumber: '8',
-        },
-        {
-          authorName: '박건우',
-          authorNumber: '9',
-        },
-        {
-          authorName: '박세종',
-          authorNumber: '10',
-        },
-        {
-          authorName: '박윤정',
-          authorNumber: '11',
-        },
-        {
-          authorName: '유경민',
-          authorNumber: '13',
-        },
-        {
-          authorName: '윤설빈',
-          authorNumber: '14',
-        },
-        {
-          authorName: '신희원',
-          authorNumber: '12',
-        },
-        {
-          authorName: '윤승규',
-          authorNumber: '15',
-        },
-        {
-          authorName: '이소윤',
-          authorNumber: '17',
-        },
-        {
-          authorName: '이도연',
-          authorNumber: '16',
-        },
-        {
-          authorName: '이승원',
-          authorNumber: '18',
-        },
-        {
-          authorName: '이은빈',
-          authorNumber: '19',
-        },
-        {
-          authorName: '장연재',
-          authorNumber: '20',
-        },
-        {
-          authorName: '장이준',
-          authorNumber: '21',
-        },
-        {
-          authorName: '조아현',
-          authorNumber: '22',
-        },
-        {
-          authorName: '최병권',
-          authorNumber: '23',
-        },
-        {
-          authorName: '최영식',
-          authorNumber: '24',
-        },
-        {
-          authorName: '한정은',
-          authorNumber: '25',
-        },
-        {
-          authorName: '홍석기',
-          authorNumber: '26',
-        },
+        '일공팔',
+        '김서현',
+        '김양지',
+        '김연수',
+        'EDDiERiNG',
+        '김평화',
+        '나수',
+        '류강희',
+        'PIAN',
+        '이우산',
+        '박윤정',
+        '신희원',
+        '유경민',
+        '호초',
+        '윤승규',
+        '이도연',
+        '이소윤',
+        '이승원',
+        '이은빈',
+        '장연재',
+        '장이준',
+        '조아현',
+        '최병권',
+        '최영식',
+        '한정은',
+        '홍석기',
       ],
     },
     B: {
       professor: '이종범',
       students: [
-        {
-          authorName: '김민성',
-          authorNumber: '29',
-        },
-        {
-          authorName: '권소현',
-          authorNumber: '28',
-        },
-        {
-          authorName: '강한별',
-          authorNumber: '27',
-        },
-        {
-          authorName: '김병건',
-          authorNumber: '30',
-        },
-        {
-          authorName: '김시윤',
-          authorNumber: '31',
-        },
-        {
-          authorName: '김여진',
-          authorNumber: '32',
-        },
-        {
-          authorName: '김현정',
-          authorNumber: '34',
-        },
-        {
-          authorName: '김윤지',
-          authorNumber: '33',
-        },
-        {
-          authorName: '노권승',
-          authorNumber: '35',
-        },
-        {
-          authorName: '박순빈',
-          authorNumber: '36',
-        },
-        {
-          authorName: '배윤지',
-          authorNumber: '37',
-        },
-        {
-          authorName: '서승환',
-          authorNumber: '38',
-        },
-        {
-          authorName: '염경민',
-          authorNumber: '40',
-        },
-        {
-          authorName: '이수아',
-          authorNumber: '42',
-        },
-        {
-          authorName: '윤종흔',
-          authorNumber: '41',
-        },
-        {
-          authorName: '이시우',
-          authorNumber: '43',
-        },
-        {
-          authorName: '송민주',
-          authorNumber: '39',
-        },
-        {
-          authorName: '이유민',
-          authorNumber: '45',
-        },
-        {
-          authorName: '이유정',
-          authorNumber: '46',
-        },
-        {
-          authorName: '이진희',
-          authorNumber: '47',
-        },
-        {
-          authorName: '이현용',
-          authorNumber: '48',
-        },
-        {
-          authorName: '이예은',
-          authorNumber: '44',
-        },
-        {
-          authorName: '정순기',
-          authorNumber: '50',
-        },
-        {
-          authorName: '장아원',
-          authorNumber: '49',
-        },
-        {
-          authorName: '황수빈',
-          authorNumber: '53',
-        },
-        {
-          authorName: '제보령',
-          authorNumber: '51',
-        },
-        {
-          authorName: '한성현',
-          authorNumber: '52',
-        },
+        '강한별',
+        '권소현',
+        '이육빵오',
+        '김병건',
+        '김시윤',
+        '김여진',
+        '김윤지',
+        '김현정',
+        '노권승',
+        '박순빈',
+        '배윤지',
+        '서승환',
+        '자란망고',
+        '염경민',
+        '윤종흔',
+        '이수아',
+        '이시우',
+        '이호글',
+        '이유민',
+        '이유정',
+        '이진희',
+        '이현용',
+        '장아원',
+        'MSG',
+        '제보령',
+        '한성현',
+        '황수빈',
       ],
     },
     C: {
       professor: '한혜연',
       students: [
-        {
-          authorName: '김소희',
-          authorNumber: '58',
-        },
-        {
-          authorName: '고선영',
-          authorNumber: '56',
-        },
-        {
-          authorName: '강서현',
-          authorNumber: '54',
-        },
-        {
-          authorName: '강지아',
-          authorNumber: '55',
-        },
-        {
-          authorName: '김기범',
-          authorNumber: '57',
-        },
-        {
-          authorName: '김유진A',
-          authorNumber: '59',
-        },
-        {
-          authorName: '김정은',
-          authorNumber: '60',
-        },
-        {
-          authorName: '박나리',
-          authorNumber: '63',
-        },
-        {
-          authorName: '김혜안',
-          authorNumber: '62',
-        },
-        {
-          authorName: '김주은',
-          authorNumber: '61',
-        },
-        {
-          authorName: '이선교',
-          authorNumber: '67',
-        },
-        {
-          authorName: '이정현',
-          authorNumber: '68',
-        },
-        {
-          authorName: '안해진',
-          authorNumber: '66',
-        },
-        {
-          authorName: '박은수',
-          authorNumber: '64',
-        },
-        {
-          authorName: '송예진',
-          authorNumber: '65',
-        },
-        {
-          authorName: '이혜인',
-          authorNumber: '69',
-        },
-        {
-          authorName: '정선아',
-          authorNumber: '71',
-        },
-        {
-          authorName: '장혜림',
-          authorNumber: '70',
-        },
-        {
-          authorName: '최화정',
-          authorNumber: '75',
-        },
-        {
-          authorName: '정혜진',
-          authorNumber: '73',
-        },
-        {
-          authorName: '정이든',
-          authorNumber: '72',
-        },
-        {
-          authorName: '지수연',
-          authorNumber: '74',
-        },
-        {
-          authorName: '추세은',
-          authorNumber: '76',
-        },
+        '강서현',
+        '산택',
+        '고선영',
+        '빛깔',
+        '유목',
+        '김유진A',
+        '김정은',
+        '김주은',
+        '홀씨',
+        '박나리',
+        '마주',
+        '야진',
+        '소솜',
+        '이정현',
+        '이혜인',
+        '건일',
+        '정선아',
+        '인멸윤금',
+        '정혜진',
+        '지수연',
+        '다비',
+        '반조',
       ],
     },
     D: {
       professor: '박병규',
       students: [
-        {
-          authorName: '기하연',
-          authorNumber: '77',
-        },
-        {
-          authorName: '김지선',
-          authorNumber: '81',
-        },
-        {
-          authorName: '김선우',
-          authorNumber: '79',
-        },
-        {
-          authorName: '김수완',
-          authorNumber: '80',
-        },
-        {
-          authorName: '김민경',
-          authorNumber: '78',
-        },
-        {
-          authorName: '김진완',
-          authorNumber: '85',
-        },
-        {
-          authorName: '김희연',
-          authorNumber: '87',
-        },
-        {
-          authorName: '남승현',
-          authorNumber: '88',
-        },
-        {
-          authorName: '김태람',
-          authorNumber: '86',
-        },
-        {
-          authorName: '김지영',
-          authorNumber: '82',
-        },
-        {
-          authorName: '김지현',
-          authorNumber: '84',
-        },
-        {
-          authorName: '김지은',
-          authorNumber: '83',
-        },
-        {
-          authorName: '민소윤',
-          authorNumber: '89',
-        },
-        {
-          authorName: '박세연',
-          authorNumber: '90',
-        },
-        {
-          authorName: '백수민',
-          authorNumber: '91',
-        },
-        {
-          authorName: '전명훈',
-          authorNumber: '94',
-        },
-        {
-          authorName: '신지민',
-          authorNumber: '92',
-        },
-        {
-          authorName: '이동현',
-          authorNumber: '93',
-        },
-        {
-          authorName: '전해인',
-          authorNumber: '95',
-        },
-        {
-          authorName: '정윤상',
-          authorNumber: '97',
-        },
-        {
-          authorName: '정나라',
-          authorNumber: '96',
-        },
-        {
-          authorName: '홍동진',
-          authorNumber: '98',
-        },
+        '이묘',
+        '김민경',
+        '김선우',
+        '구망할',
+        '도마',
+        '김지영',
+        '김지은',
+        'COMMA',
+        '김진완',
+        '김태람',
+        '뀰 (kkyul)',
+        '남승현',
+        '민소윤',
+        '박세연',
+        '백수민',
+        '훈이',
+        '이동현',
+        '전명훈',
+        '전해인',
+        '정나라',
+        '정상',
+        '홍동진',
       ],
     },
     E: {
       professor: '양혜림',
       students: [
-        {
-          authorName: '김가란',
-          authorNumber: '100',
-        },
-        {
-          authorName: '구현진',
-          authorNumber: '99',
-        },
-        {
-          authorName: '김은혜',
-          authorNumber: '101',
-        },
-        {
-          authorName: '김재희',
-          authorNumber: '102',
-        },
-        {
-          authorName: '김지원',
-          authorNumber: '103',
-        },
-        {
-          authorName: '김현경',
-          authorNumber: '104',
-        },
-        {
-          authorName: '박소현',
-          authorNumber: '107',
-        },
-        {
-          authorName: '박소정',
-          authorNumber: '106',
-        },
-        {
-          authorName: '김현지',
-          authorNumber: '105',
-        },
-        {
-          authorName: '박지원',
-          authorNumber: '108',
-        },
-        {
-          authorName: '서예람',
-          authorNumber: '109',
-        },
-        {
-          authorName: '송동현',
-          authorNumber: '110',
-        },
-        {
-          authorName: '이나리',
-          authorNumber: '112',
-        },
-        {
-          authorName: '이수현',
-          authorNumber: '113',
-        },
-        {
-          authorName: '이슬아',
-          authorNumber: '114',
-        },
-        {
-          authorName: '이아영',
-          authorNumber: '116',
-        },
-        {
-          authorName: '심다연',
-          authorNumber: '111',
-        },
-        {
-          authorName: '이시현',
-          authorNumber: '115',
-        },
-        {
-          authorName: '이여경',
-          authorNumber: '118',
-        },
-        {
-          authorName: '이은지',
-          authorNumber: '119',
-        },
-        {
-          authorName: '최연우',
-          authorNumber: '123',
-        },
-        {
-          authorName: '임희정',
-          authorNumber: '121',
-        },
-        {
-          authorName: '장은진',
-          authorNumber: '122',
-        },
-        {
-          authorName: '이찬영',
-          authorNumber: '120',
-        },
-        {
-          authorName: '이애진',
-          authorNumber: '117',
-        },
-        {
-          authorName: '허예은',
-          authorNumber: '124',
-        },
+        '구현진',
+        '김가란',
+        '김은혜',
+        '김재희',
+        '김지원',
+        '김현경',
+        '김현지',
+        '박소정',
+        '주화',
+        '박지원',
+        '서예람',
+        '택이',
+        '심다연',
+        '이나리',
+        '이수현',
+        '이슬아',
+        '이시현',
+        '이아영',
+        '안진애',
+        '이여경',
+        '이은지',
+        '이찬영',
+        '시혜',
+        '장은진',
+        '최연우',
+        '허예은',
       ],
     },
     F: {
       professor: '문아름',
       students: [
-        {
-          authorName: '권구민',
-          authorNumber: '126',
-        },
-        {
-          authorName: '강건욱',
-          authorNumber: '125',
-        },
-        {
-          authorName: '김세윤',
-          authorNumber: '127',
-        },
-        {
-          authorName: '김에더',
-          authorNumber: '128',
-        },
-        {
-          authorName: '김예림',
-          authorNumber: '130',
-        },
-        {
-          authorName: '김혜원',
-          authorNumber: '132',
-        },
-        {
-          authorName: '김연주',
-          authorNumber: '129',
-        },
-        {
-          authorName: '김은정',
-          authorNumber: '131',
-        },
-        {
-          authorName: '서은진',
-          authorNumber: '137',
-        },
-        {
-          authorName: '박승현',
-          authorNumber: '134',
-        },
-        {
-          authorName: '배이솔',
-          authorNumber: '136',
-        },
-        {
-          authorName: '신현아',
-          authorNumber: '138',
-        },
-        {
-          authorName: '유예랑',
-          authorNumber: '140',
-        },
-        {
-          authorName: '문슬기',
-          authorNumber: '133',
-        },
-        {
-          authorName: '오지애',
-          authorNumber: '139',
-        },
-        {
-          authorName: '박용호',
-          authorNumber: '135',
-        },
-        {
-          authorName: '차다빈',
-          authorNumber: '145',
-        },
-        {
-          authorName: '조수미',
-          authorNumber: '143',
-        },
-        {
-          authorName: '장지원',
-          authorNumber: '142',
-        },
-        {
-          authorName: '최지은',
-          authorNumber: '146',
-        },
-        {
-          authorName: '진희원',
-          authorNumber: '144',
-        },
-        {
-          authorName: '한평선',
-          authorNumber: '147',
-        },
-        {
-          authorName: '홍은표',
-          authorNumber: '148',
-        },
-        {
-          authorName: '이아름',
-          authorNumber: '141',
-        },
+        '강건욱',
+        '권구민',
+        '김찰찰',
+        '김에더',
+        '김연주',
+        '김예림',
+        '김은정',
+        '베네드',
+        '문슬기',
+        '호',
+        '엘비씨',
+        '마당',
+        '도토람',
+        '신현아',
+        '오지애',
+        '유예랑',
+        '이아름',
+        '장지원',
+        '조수미',
+        '진희원',
+        '차다빈',
+        '최지은',
+        '한평선',
+        '홍은표',
       ],
     },
     G: {
       professor: '이지훈',
       students: [
-        {
-          authorName: '김남준',
-          authorNumber: '149',
-        },
-        {
-          authorName: '김민주',
-          authorNumber: '150',
-        },
-        {
-          authorName: '백가람',
-          authorNumber: '154',
-        },
-        {
-          authorName: '김유진B',
-          authorNumber: '151',
-        },
-        {
-          authorName: '오유식',
-          authorNumber: '157',
-        },
-        {
-          authorName: '송다현',
-          authorNumber: '155',
-        },
-        {
-          authorName: '김진석',
-          authorNumber: '152',
-        },
-        {
-          authorName: '양다빈',
-          authorNumber: '156',
-        },
-        {
-          authorName: '이민정',
-          authorNumber: '158',
-        },
-        {
-          authorName: '박성우',
-          authorNumber: '153',
-        },
-        {
-          authorName: '이소영',
-          authorNumber: '159',
-        },
-        {
-          authorName: '이수림',
-          authorNumber: '161',
-        },
-        {
-          authorName: '정찬솔',
-          authorNumber: '166',
-        },
-        {
-          authorName: '장예진',
-          authorNumber: '165',
-        },
-        {
-          authorName: '이수민',
-          authorNumber: '162',
-        },
-        {
-          authorName: '이혜연',
-          authorNumber: '164',
-        },
-        {
-          authorName: '이지현',
-          authorNumber: '163',
-        },
-        {
-          authorName: '함세주',
-          authorNumber: '167',
-        },
-        {
-          authorName: '이소정',
-          authorNumber: '160',
-        },
-        {
-          authorName: '홍윤지',
-          authorNumber: '169',
-        },
-        {
-          authorName: '허재혁',
-          authorNumber: '168',
-        },
+        '김남준',
+        '김민주',
+        '김유진B',
+        '김진석',
+        '박성우',
+        '백가람',
+        '송다현',
+        '눈사람',
+        '오유식',
+        '이민정',
+        '이소영',
+        '이소정',
+        '이수림',
+        '이수민',
+        '호뱍',
+        '이리건',
+        '장예진',
+        '국산인간',
+        '함세주',
+        '허재혁',
+        '표독',
       ],
     },
     H: {
       professor: '김은권',
-      students: [
-        {
-          authorName: '김정인',
-          authorNumber: '170',
-        },
-        {
-          authorName: '허수정',
-          authorNumber: '171',
-        },
-      ],
+      students: ['김정인', '허수정'],
     },
   },
 };
