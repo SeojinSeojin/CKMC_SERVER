9ece7c53cdd934617f7a505072bf16eb1636ed14
seojin kim <001106ksj@gmail.com>
2022-01-04 16:34:11 +0900

    feat: 댓글 관련 controller 작성

--- src/controllers/comments.controller.ts
+++ src/controllers/comments.controller.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Response } from 'express';
+import { NextFunction, Response, Request } from 'express';
 import { Author, Comment, Episode, User, Work } from '../db/schema';
 import { CommentData, EpisodeData } from '../types';
 import { CustomRequest } from '../types/API';
@@ -33,36 +33,55 @@ export const postComments = async (
 
   await comment.save();
 
-  await Episode.updateOne(
-    { authorName, index: episodeIndex },
-    {
-      $push: { comments: comment },
-    }
-  );
-
-  const episodes = author.work.episodes;
-  const newEpisodes = episodes.map((episode: EpisodeData) => {
-    if (episode.index === episodeIndex)
-      return {
-        ...episode,
-        comments: episode.comments ? [...episode.comments, comment] : [comment],
-      };
-    else return episode;
-  });
-
-  await Work.updateOne(
-    { authorName: author.nickName },
-    { $set: { episodes: newEpisodes } }
-  );
-  const work = await Work.findOne({ authorName: author.nickName });
-
-  await Author.updateOne({ _id: author._id }, { $set: { work: work } });
-  const newAuthor = await Author.findOne({ _id: author._id });
-
-  await User.updateOne(
-    { 'author._id': author._id },
-    { $set: { author: newAuthor } }
-  );
-
   return res.status(200).json({ comment });
+};
+
+export const getCommentByID = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { username, password, commentID } = req.query;
+  if (!username || !password || !commentID)
+    return res.status(400).json({ message: '잘못된 요청 형식입니다.' });
+
+  const comment: CommentData = await Comment.findById(commentID);
+  if (comment.username !== username)
+    return res.status(400).json({ message: '잘못된 유저이름입니다.' });
+  if (comment.password !== password)
+    return res.status(400).json({ message: '잘못된 비밀번호입니다.' });
+  return res.status(200).json(comment);
+};
+
+export const getCommentsByEpisode = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { authorName, episodeIndex } = req.query;
+  if (!authorName || !episodeIndex)
+    return res.status(400).json({ message: '잘못된 요청 형식입니다.' });
+  const comments: CommentData[] = await Comment.find({
+    authorName,
+    episodeIndex,
+  });
+  return res.status(200).json(comments);
+};
+
+export const patchComment = async (
+  req: CustomRequest<CommentData>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { username, password, _id, content } = req.body;
+  if (!username || !password || !_id)
+    return res.status(400).json({ message: '잘못된 요청 형식입니다.' });
+
+  const comment: CommentData = await Comment.findById(_id);
+  if (comment.username !== username)
+    return res.status(400).json({ message: '잘못된 유저이름입니다.' });
+  if (comment.password !== password)
+    return res.status(400).json({ message: '잘못된 비밀번호입니다.' });
+  await Comment.updateOne({ _id }, { $set: { content } });
+  return res.status(200).json(comment);
 };
